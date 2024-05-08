a36bf51a98765fe44f8827aff008b54b8c2142af
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-08 06:07:03 -0700

    Python if loops Task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,5 @@ for charpos in range(ord('a'), ord('z')+1):
     char = chr(charpos)
     if charpos == ord('e') or ord('q'):
         continue
-    print("{}".format(char), end='')
+    else:
+        print("{}".format(char), end='')
