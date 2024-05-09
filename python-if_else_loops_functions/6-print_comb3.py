066450a2e64cabb0691373789e5673baace30e0d
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-09 10:14:17 -0700

    Python If Loops Task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for dec in range(0, 9):
     for n in range(1, 10):
         if dec > n or dec == n:
             continue
-        if n == 9 and dec ==8:
-            print ("{}{}" .format(dec, n))
+        if n == 9 and dec == 8:
+            print("{}{}" .format(dec, n))
         else:
-            print ("{}{}, " .format(dec, n), end="")
+            print("{}{}, " .format(dec, n), end="")
