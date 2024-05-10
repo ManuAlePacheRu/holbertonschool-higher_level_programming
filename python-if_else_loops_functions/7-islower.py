cd9388f5c4c65531afd875a205b2e3524b790ced
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-10 06:31:16 -0700

    Python If Loops Task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-        # print(type(c))
-        # print(isinstance(c, str))
-        if ord(c) <= 122 and ord(c) >= 97:
+    if ord(c) <= 122 and ord(c) >= 97:
             return True
-        return False
+    return False
