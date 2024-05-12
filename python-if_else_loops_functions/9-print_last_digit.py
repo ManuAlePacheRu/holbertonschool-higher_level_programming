1f972fdb678b4c8bebf88b08c7172e1aeccf3c9f
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-11 19:07:11 -0500

    pycodestyle fix Task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-  lastdig = abs(number) % 10
-  print("{:d}".format(lastdig), end='')
-  return (lastdig)
+    lastdig = abs(number) % 10
+    print("{:d}".format(lastdig), end='')
+    return (lastdig)
