9d138cdac87a4bc549dc2a5abfd3bb22ae3c0be5
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-13 09:44:31 -0500

    Not Showing idx content correctly Fix

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx not in my_list:
-        return None
+    if idx in range(-len(my_list),(len(my_list))):
+        return my_list[idx]
     else:
-        return idx +1
+        return None
+
