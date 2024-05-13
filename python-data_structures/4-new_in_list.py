733889f44a591e4dbf0e23c1b92515a84db23d7c
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-13 12:52:31 -0500

    copy list Checker

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copylist = my_list
+    copylist = my_list.copy()
     if idx < 0 or idx >= len(my_list):
         return copylist
     else: 
