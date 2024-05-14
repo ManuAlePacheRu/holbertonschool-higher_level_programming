22849fa1b91e73488f743683a311525abbf7f8b5
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-14 13:10:27 -0500

    pycodestyle Fix Task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         return 0, None
-    else:  
+    else:
         length = len(sentence)
         first = sentence[0]
         return length, first
