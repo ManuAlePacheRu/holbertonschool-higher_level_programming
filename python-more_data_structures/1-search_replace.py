cae37f40bd0fc7a8432735ecbef196c5f040c3d1
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-15 12:48:45 -0500

    2 Unused Variables Fix Task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     copylist = my_list.copy()
     for numrep in range(len(copylist)):
-        if copylist[numrep] == 2:
-            copylist[numrep] = 89
+        if copylist[numrep] == search:
+            copylist[numrep] = replace
     return copylist
