85287168180d49247df781cf710bf2096477769c
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-17 09:13:10 -0500

    Only in 3 lines fix Task 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-   copylist = my_list.copy()
-   return list(map(lambda nummul: nummul * number, copylist))
+   return list(map(lambda nummul: nummul * number, my_list[:]))
