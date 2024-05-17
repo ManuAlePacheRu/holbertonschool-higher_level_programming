8f0cc74584ddcff51c52323066cb99728b295a9c
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-17 08:03:24 -0500

    Python M Data Structures Task 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    laux = []
+    for score in a_dictionary.values():
+        laux.append(score) 
+    return max(laux)
