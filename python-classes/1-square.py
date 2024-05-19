ffe5bb81883be2656894cea536a61a060fe475ad
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-19 11:32:40 -0500

    Classes Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3  # This line is called a shebang. It tells the system this script should be run with Python3.
+"""Square class with size private atribute in instance"""
+class Square:  # This line defines a new class named Square.
+    """Square class with size private atribute in instance"""
+    def __init__(self, size):
+        self.__size = size
