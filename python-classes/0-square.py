6081809224f1ef354f296b0d9341625356092072
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-19 11:11:08 -0500

    Comment correction to see if it complete checker requirements

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,4 @@
-#!/usr/bin/python3
-class Square: #Class Definition
-    pass #Pass to skip empty class here
+#!/usr/bin/python3  # This line is called a shebang. It tells the system this script should be run with Python3.
+
+class Square:  # This line defines a new class named Square.
+    pass  # The 'pass' keyword is a placeholder. It means "do nothing". It's used here because Python syntax requires at least one statement in the class.
