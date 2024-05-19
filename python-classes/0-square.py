df9ac9cf31e86950d42c8ef36abd89d6fb711723
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-19 11:08:58 -0500

    Pycodestyle error fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 class Square: #Class Definition
-    pass #Pass to tell in this case to the program that it have to skip this empty class
+    pass #Pass to skip empty class here
