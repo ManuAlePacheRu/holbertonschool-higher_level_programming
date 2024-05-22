218b653e6886bd490055efadfb1e6653f351b75e
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-22 08:50:02 -0500

    pycodestyle Fix Task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ This is a module that print first and last name """
+
+
 def say_my_name(first_name, last_name=""):
     """
     Print the provided first and last names.
@@ -14,10 +16,10 @@ def say_my_name(first_name, last_name=""):
     """ Check if first_name is a string """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    
+
     """ Check if last_name is a string """
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     """ Print the name """
     print("My name is {} {}".format(first_name, last_name))
