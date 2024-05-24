28281e0a5709a875ea6d55ed084d4563a1a933a2
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-24 08:25:17 -0500

    Add Test and Documentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-This is a method that aplies a tab to a text when it find [".", "?", ":"]
+This is a method that applies indentation to a text when it finds [".", "?", ":"]
 """
+
+
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for i in text:
-        print(i, end="")
+        print( i, end="")
         if i == "." or i == "?" or i == ":":
             print()
             print()
-    else:
-        print(i, end="")
+  
