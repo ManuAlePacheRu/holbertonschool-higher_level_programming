75a3d611d7e4299ecb5480ec09bf9bc3eb07169e
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-24 08:29:26 -0500

    More Documentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,6 +5,18 @@ This is a method that applies indentation to a text when it finds [".", "?", ":"
 
 
 def text_indentation(text):
+    """
+    Apply indentation to text based on certain characters.
+
+    Args:
+        text (str): The input text to be indented.
+
+    Raises:
+        TypeError: If `text` is not a string.
+
+    Returns:
+        None
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for i in text:
