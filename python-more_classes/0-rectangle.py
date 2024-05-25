24e168a7d9ef49a75b9ec7f6f95604f4d607129f
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-25 08:57:56 -0500

    Python More Classes Task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3    # This line is called a shebang. It tells the system this script should be run with Python3.
+"""
+Empty Rectangle  class
+"""
+
+
+class Rectangle:    # This line defines a new class named Rectangle.
+    """
+    Empty Rectangle  class
+    """
+    pass    # The 'pass' keyword is a placeholder. It means "do nothing".
