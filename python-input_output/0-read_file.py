b391e4cdd3829c1ae07a4da00af67d652c1824bd
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-27 08:41:43 -0500

    Documentation added.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+
 def read_file(filename=""):
+    """
+    Read a text file and print its content to stdout.
+
+    Args:
+        filename (str): The path to the file to be read. Defaults to an empty string.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+    """
     with open(filename, 'r', encoding="utf-8") as file:
         for line in file:
             print(line, end='')
