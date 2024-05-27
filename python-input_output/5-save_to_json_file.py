4d52c0206829b020c7d43f182997d68a82f5ff2b
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-27 11:32:27 -0500

    Python Input Output Task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write an object to a JSON file."""
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    Write an object to a JSON file.
+
+    Args:
+        my_obj: The Python object to be written to the file.
+        filename (str): The name of the file to write to.
+
+    Returns:
+        None
+
+    Raises:
+        IOError: If the file cannot be opened or written to.
+    """
+    with open(filename, 'w', encoding="utf-8") as filewrite:
+        json.dump(my_obj, filewrite)
