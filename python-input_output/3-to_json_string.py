5a12eb381309e71abc1a34c9ed1aadfa6505afa3
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-27 10:51:32 -0500

    Python Input Output Task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import json
+"""
+Convert a Python object to a JSON string.
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Convert a Python object to a JSON string.
+
+    Args:
+        my_obj: The Python object to be converted to JSON.
+
+    Returns:
+        str: The JSON representation of the input Python object.
+
+    Raises:
+        TypeError: If the input object cannot be serialized to JSON.
+    """
+    return json.dumps(my_obj)
