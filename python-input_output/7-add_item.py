082f153a0247472b49c2f7a7f24c2522d253e424
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-28 13:49:02 -0500

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,3 +1,29 @@
+#!/usr/bin/python3
+import os
 import json
+from sys import argv
 
-def load_add_save():
+"""すべての引数をPythonリストに書き込んでファイルに保存します"""
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+argvs = argv[1:]
+arg_list = []
+
+# 引数をリストに追加
+for i in (argvs):
+    arg_list.append(i)
+
+if os.path.isfile(filename):
+    # ファイルが存在する場合は、その内容を読み込んで追加
+    arg_listy = load_from_json_file(filename)
+    print(type(arg_list))
+    for newelem in (arg_list):
+        arg_listy.append(newelem)
+    save_to_json_file(arg_listy, filename)
+else:
+    # ファイルが存在しない場合は、新しいリストを保存
+    print(arg_list)
+    save_to_json_file(arg_list, filename)
