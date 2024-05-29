72b805710fed903c376be3fa252e35b5757b558b
ManuAlePacheRu <manualepacheru@gmail.com>
2024-05-29 08:12:10 -0500

    Fix Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -19,7 +19,6 @@ for i in (argvs):
 if os.path.isfile(filename):
     # ファイルが存在する場合は、その内容を読み込んで追加
     arg_listy = load_from_json_file(filename)
-    print(type(arg_list))
     for newelem in (arg_list):
         arg_listy.append(newelem)
     save_to_json_file(arg_listy, filename)
