35763489dc2b6fe8d5bdccebf81b9c920aee0e24
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-19 10:57:35 -0700

    Parentesis Fixed

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -3,8 +3,7 @@
 SET @database_name_get := DATABASE()
 SET @create_table_new := CONCAT('CREATE TABLE IF NOT EXISTS ', @database_name_get, '.first_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-)');
+    name VARCHAR(256) NOT NULL)');
 PREPARE create_table FROM @create_table_new;
 EXECUTE create_table;
 DEALLOCATE PREPARE create_table;
