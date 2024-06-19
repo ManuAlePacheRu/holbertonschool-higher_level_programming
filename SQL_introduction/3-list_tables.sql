03b1d71c7bf4bedc42bc83a4356370ffe5279642
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-19 09:28:16 -0700

    Task 3 variables name changed

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,6 +1,6 @@
 -- List all tables from a database passed as argument
 
-SET @sql := CONCAT('SHOW TABLES IN `', DATABASE(), '`');
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+SET @operation := CONCAT('SHOW TABLES IN `', DATABASE(), '`');
+PREPARE consult FROM @operation;
+EXECUTE consult;
+DEALLOCATE PREPARE consult;
