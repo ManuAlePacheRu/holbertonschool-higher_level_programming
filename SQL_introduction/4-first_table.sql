7316a62813d850d45e0acccf2b56fbee7b02430d
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-19 10:38:09 -0700

    Task 4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,10 @@
+-- Create a table in the database passed as argument
+
+SET @database_name_get := DATABASE()
+SET @create_table_new := CONCAT('CREATE TABLE IF NOT EXISTS ', @database_name, '.first_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+)');
+PREPARE create_table FROM @create_table_new;
+EXECUTE create_table;
+DEALLOCATE PREPARE create_table;
