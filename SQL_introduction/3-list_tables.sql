878a65fa812652c482524d7a1ce2498d436f4ab4
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-19 18:23:31 +0000

    Task 3 and 4 fix

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,6 +1,3 @@
 -- List all tables from a database passed as argument
 
-SET @operation := CONCAT('SHOW TABLES IN `', DATABASE(), '`');
-PREPARE consult FROM @operation;
-EXECUTE consult;
-DEALLOCATE PREPARE consult;
+SHOW TABLES;
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,9 +1,14 @@
 -- Create a table in the database passed as argument
 
-SET @database_name_get := DATABASE()
-SET @create_table_new := CONCAT('CREATE TABLE IF NOT EXISTS ', @database_name_get, '.first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL)');
-PREPARE create_table FROM @create_table_new;
-EXECUTE create_table;
-DEALLOCATE PREPARE create_table;
+    name VARCHAR(256) NOT NULL
+);
+
+-- SET @database_name_get := DATABASE()
+-- SET @create_table_new := CONCAT('CREATE TABLE IF NOT EXISTS ', @database_name_get, '.first_table (
+-- id INT AUTO_INCREMENT PRIMARY KEY,
+-- name VARCHAR(256) NOT NULL)');
+-- PREPARE create_table FROM @create_table_new;
+-- EXECUTE create_table;
+-- DEALLOCATE PREPARE create_table;
