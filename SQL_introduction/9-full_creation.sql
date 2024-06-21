8fcc0a1cac5297d9118e692ac077667921dc0f31
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-21 13:46:24 +0000

    Task 8 and 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Scrip that create a second_table and insert records
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    score INT
+)
+
+INSERT INTO second_table (id, name, score)
+VALUES(1, 'John', 10)
+      (2, 'Alex', 3)
+      (3, 'Bob', 14)
+      (4, 'George', 8)
