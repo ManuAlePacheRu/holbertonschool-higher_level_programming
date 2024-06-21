acb964a1ba698635eff19e90c7a4e8e73dc41c9b
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-21 19:02:34 +0000

    Task 1 Fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- This Scrip create a user with all privilieges
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+ALTER USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
