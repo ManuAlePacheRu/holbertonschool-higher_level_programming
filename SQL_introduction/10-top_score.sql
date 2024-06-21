e36a51e567dd1842c4f32ede21dbeb6f06e6e592
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-21 14:26:28 +0000

    Task 9 Fix name not id

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Script that lists all records of the second_table of in asc order
 
-SELECT score, id FROM second_table ORDER BY score ASC;
+SELECT score, name FROM second_table ORDER BY score ASC;
