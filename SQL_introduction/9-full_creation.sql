18c95a866120552d4c6c614b8f971e008a631a4b
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-21 13:55:53 +0000

    Task 9 ";" Fix at the end of table creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     score INT
-)
+);
 
 INSERT INTO second_table (id, name, score)
 VALUES(1, 'John', 10)
