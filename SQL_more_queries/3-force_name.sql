a9bbd8e454d202e5db9091cc3c00a740758a3636
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-23 17:00:11 +0000

    Task 3 Fix not auto increment

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- This create a table with the records id and name
 
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
