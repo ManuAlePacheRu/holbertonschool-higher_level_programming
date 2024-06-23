23e76c440ce2bf7e340391af4403adf00444a247
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-23 16:48:08 +0000

    Task 2 MySQL

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- This scrip creates a database a suer with password and gran select acces
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
