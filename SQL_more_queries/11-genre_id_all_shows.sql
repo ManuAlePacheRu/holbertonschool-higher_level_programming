e4ac8a6fa64c6f063a2d93da7493dcfa645dadd2
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-24 16:59:27 +0000

    Task 11 NULL Fix

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Esta consulta lista todos los títulos de programas de televisión junto con sus IDs de género asociados, o 'NULL' si no tienen género.
 
-SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, 'NULL') AS genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
