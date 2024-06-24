8506c127958fcc6aa941782135f5367f48d7b860
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-24 16:24:56 +0000

    Task 11 Fix

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,4 +3,4 @@
 SELECT tv_shows.title, COALESCE(tv_show_genres.id, 'NULL') AS id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, genre_id ASC;
+ORDER BY tv_shows.title ASC, id ASC;
