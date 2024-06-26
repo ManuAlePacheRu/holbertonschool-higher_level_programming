74b638c7156d7e4ff8566e4433790e5c5dc44e00
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-26 13:31:59 -0300

    Code not executed when impor Fix Task 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,40 @@
+# Establish connection to MySQL database using command-line arguments
 import MySQLdb  # Import MySQLdb library for MySQL database interaction
 import sys  # Import sys module for accessing command-line arguments
 
-# Establish connection to MySQL database using command-line arguments
-conn = MySQLdb.connect(
-    host="localhost",  # MySQL server host
-    port=3306,  # MySQL server port
-    user=sys.argv[1],  # MySQL username (provided as command-line argument)
-    passwd=sys.argv[2],  # MySQL password (provided as command-line argument)
-    db=sys.argv[3],  # MySQL database name (provided as command-line argument)
-    charset="utf8"  # Character encoding for the connection
-)
+def get_states():
+    """
+    Connects to a MySQL database and retrieves all states from the 'states' table.
 
-# Create a cursor object to execute SQL queries
-cur = conn.cursor()
+    Assumes MySQL username, password, and database name are provided as command-line arguments.
+    """
+    conn = MySQLdb.connect(
+        host="localhost",  # MySQL server host
+        port=3306,  # MySQL server port
+        user=sys.argv[1],  # MySQL username (provided as command-line argument)
+        passwd=sys.argv[2],  # MySQL password (provided as command-line argument)
+        db=sys.argv[3],  # MySQL database name (provided as command-line argument)
+        charset="utf8"  # Character encoding for the connection
+    )
 
-# Execute SQL query to select all rows from the 'states' table
-cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # Create a cursor object to execute SQL queries
+    cur = conn.cursor()
 
-# Fetch all rows from the executed query
-query_rows = cur.fetchall()
+    # Execute SQL query to select all rows from the 'states' table
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-# Iterate over fetched rows and print each row
-for row in query_rows:
-    print(row)
+    # Fetch all rows from the executed query
+    query_rows = cur.fetchall()
 
-# Close the cursor to release resources
-cur.close()
+    # Iterate over fetched rows and print each row
+    for row in query_rows:
+        print(row)
 
-# Close the database connection
-conn.close()
+    # Close the cursor to release resources
+    cur.close()
+
+    # Close the database connection
+    conn.close()
+
+if __name__ == '__main__':
+    get_states()
