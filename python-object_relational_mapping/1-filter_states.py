1a9cf24d36d18a280e357c0e50bafd93b0912522
ManuAlePacheRu <manualepacheru@gmail.com>
2024-06-26 14:47:15 -0300

    Task 2 Mayus N Fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -30,7 +30,7 @@ def get_states():
     cur = conn.cursor()
     
     # Execute SQL query to select states starting with 'N'
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
 
     # Fetch all rows from the executed query
     query_rows = cur.fetchall()
