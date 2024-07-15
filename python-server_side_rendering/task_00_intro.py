7d7b3641d626c49318cf0edcc8e0f1cdde1b16ad
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-15 15:01:25 -0300

    def check_make_file position Fix Task 0

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -26,8 +26,8 @@ def generate_invitations(template, attendees):
         inv.append(new_template)
         return inv
 
-    def check_make_file(num):
-        for idx, inv in enumerate(inv, start=1):
-            output_file = f"output_{idx}.txt"
-            with open(output_file, 'w') as file:
-                file.write(inv)
+def check_make_file(num):
+    for idx, inv in enumerate(inv, start=1):
+        output_file = f"output_{idx}.txt"
+        with open(output_file, 'w') as file:
+            file.write(inv)
