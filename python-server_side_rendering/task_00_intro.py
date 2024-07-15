94011e10e571b16e66ab5b964e1ee4e3a65b761a
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-15 14:19:12 -0300

    Task 0 Emptiness and Types Fix

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,17 +5,23 @@ import os
 def generate_invitations(template, attendees):
     
     if not isinstance(template, str):
-        logging.error("Error: Template is not a string")
-
+        print(type(template).__name__)
+        return
     if not isinstance(attendees, list):
-        logging.error("Error: attendees  is not a list of dictionaries")
+        print(type(attendees).__name__)
+        return
+    for element in attendees:
+        if not isinstance(element, dict):
+            print(type(element).__name__)
+            return
 
     if not template.strip():
-        logging.error("Error: Template is empty")
-    
-    if not attendees():
-        logging.error("Error: Attendees is empty") 
+        print("Error: Template is empty")
+        return
 
+    if not attendees():
+        print("Error: Attendees is empty") 
+        return
     inv = []
 
     for attendee in attendees:
