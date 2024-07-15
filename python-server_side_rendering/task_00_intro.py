b1f482649441d804b11ce46c7c583bc5f26e6e13
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-15 15:13:20 -0300

    Task 0 Check_make_file Fix

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,5 +1,4 @@
 from string import Template
-import logging
 import os
 
 def generate_invitations(template, attendees):
@@ -26,8 +25,8 @@ def generate_invitations(template, attendees):
         inv.append(new_template)
         return inv
 
-def check_make_file(num):
-    for idx, inv in enumerate(inv, start=1):
-        output_file = f"output_{idx}.txt"
+    def check_make_file(num):
+        for idx, inv in enumerate(invitations, start=1):
+        output_file = os.path.join(output_dir, f"output_{idx}.txt")
         with open(output_file, 'w') as file:
             file.write(inv)
