df1e54d7c71cb6df00359b380a7777bf216626c0
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-19 10:25:42 -0300

    Variable Fix Task 2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,31 +1,31 @@
+#!/usr/bin/python3
+
 from flask import Flask, render_template
-import jinja2
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-   return render_template('about.html')
+    return render_template('about.html')
 
 @app.route('/contact')
 def contact():
-   return render_template('contact.html')
+    return render_template('contact.html')
 
 @app.route('/items')
 def items():
-   with open("items.json") as list_items:
-        items_data = json.load(list_items)   
-
-   if items_data:
-      items_py = items_data["items"]
-   else:
-      items_py = []
-   return render_template('items.html', items_py=items)
+    with open("items.json", "r") as file:
+        data_item = json.load(file)
+    if data_item:
+        items_lis = data_item["items"]
+    else:
+        items_lis = []
+    return render_template('items.html', items=items_lis)
 
 if __name__ == '__main__':
    app.run(debug=True, port=5000)
