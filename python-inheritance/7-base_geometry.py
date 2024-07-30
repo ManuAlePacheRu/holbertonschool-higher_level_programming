b8c79d34a3e746a4e53676aeb1d84ac9df94d732
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-30 13:43:34 -0300

    Task 07 Fix

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
         """
             Check if value is a num and if it is greater than cero
         """
-        if not isinstance(value, name, int):
-            raise TypeError("<name> must be an integer")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
