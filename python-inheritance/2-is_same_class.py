cf17f14397255584cdb44821e378d9d0e8b06258
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-30 12:44:45 -0300

    Inerithance Task 02

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    Method to check if a object is from a same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        Method Definition
+    """
+    if type(obj) is a_class:
+        return True
+    else: 
+        return False
