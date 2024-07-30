953e2555ae59a114e9376cfbc3af703ac78dcf84
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-30 13:10:17 -0300

    Inheritance Task 06

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    A class called BaseGeometry with an area method
+"""
+
+
+class BaseGeometry:
+    """
+        Class with an area method
+    """
+    
+    def area(self):
+        """
+            Exeption raised
+        """
+        raise Exception("area() is not implemented")
