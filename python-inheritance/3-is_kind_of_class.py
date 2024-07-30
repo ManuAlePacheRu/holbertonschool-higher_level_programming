ba163bee5639ae717e3c7e8af10f06eb62d43b9f
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-30 12:51:15 -0300

    Task 03 Inheritance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Check if the obj is instance of a class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Method definition and use of isinstance
+    """
+    return isinstance(obj, a_class)
